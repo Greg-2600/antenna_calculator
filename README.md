ef065bd676e9385e1d5fef74f662faa71ea9acde
gregbakerstl <gregbakerstl@gmail.com>
2019-09-26 14:10:46 +0000

    arguments, documentation, light speed relative speed

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Math for Antenna Calculations
 
 A simple calculator that given a frequency, will provide the numbers needed to cut and hang a resonante dipole antenna.
+
+ Install: 
+`pip install -r requirments.txt`
+
+ Usage:
+`python antenna-calculator.py --frequency 7.185`
--- antenna_calculator.py
+++ antenna_calculator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
+import argparse
 
 def antenna_calculator(frequency):
     light_speed_meters = float('299792458')
+    light_speed_meters_via_copper = light_speed_meters * 0.95
 
     frequency_in_kilo = frequency * 1000
 
-    wave_length_meters = light_speed_meters / frequency_in_kilo / 1000
+    wave_length_meters = light_speed_meters_via_copper / frequency_in_kilo / 1000
     wave_length_feet = wave_length_meters / 0.3048
 
     quarter_wave_length_meters = wave_length_meters * 0.25
@@ -17,6 +19,7 @@ def antenna_calculator(frequency):
     #inches = float(wave_length) / .3048 % 1 * 12
 
     print ('light speed: %s' % light_speed_meters)
+    print ('light speed via copper: %s' % light_speed_meters_via_copper)
     print ('input frequency: %s' % frequency)
     print ('frequency in kilocycles: %s' % frequency_in_kilo)
     print ('wave length in meters: %s' % wave_length_meters)
@@ -27,5 +30,7 @@ def antenna_calculator(frequency):
     print ('dipole element length in feet: %s' % dipole_element_feet)
 
 if __name__ == '__main__':
-    frequency = float('145.00')
-    antenna_calculator(frequency)
+    parser = argparse.ArgumentParser(description='calculate antenna properties by frequency')
+    parser.add_argument('frequency', type=float, help='transmission frequency')
+    args = parser.parse_args()
+    antenna_calculator(args.frequency)
